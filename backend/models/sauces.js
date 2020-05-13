3343d10ea9b83e751440bff3da078e81ea0dd357
FrenchyProds <paulolocito@hotmail.fr>
2020-05-13 11:18:06 +0100

    Sauce input validation
    Added input validation to the sauces (name, manufacturer, description and mainPepper) via custom regex AND/OR specific input types.

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const sauceValidation = require('../middleware/sauceValidation');
 
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: { type: String, required: true },
+    name: { type: String, required: true, validate : sauceValidation.nameValidator },
+    manufacturer: { type: String, required: true, validate : sauceValidation.manufacturerValidator },
+    description: { type: String, required: true, validate : sauceValidation.descriptionValidator },
+    mainPepper: { type: String, required: true, validate : sauceValidation.pepperValidator },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true, default : 0 },
     likes: { type: Number, required: false, default : 0 },
